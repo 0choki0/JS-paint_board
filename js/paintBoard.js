0e6bdb23fbe4b2f8629327765e0cf9e1af229ae2
jiho_lee <wlgh0324@gmail.com>
2024-08-07 15:29:57 +0900

    마우스로 선 그리기 기능 구현
    마우스 down 상태에서 선을 그리는 기능 구현.

--- js/paintBoard.js
+++ js/paintBoard.js
@@ -1,5 +1,37 @@
+// get html's elements
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d");
 
+// constant string
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
+let pencilMode = ""
+const DRAW = "draw"
+
+// canvas property
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+function startDraw(event) {
+    pencilMode = DRAW;
+    if (pencilMode === DRAW) {
+        ctx.moveTo(event.offsetX, event.offsetY);
+        moveMouse(event);
+    }
+}
+
+function moveMouse(event) {
+    if (pencilMode === DRAW) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        ctx.moveTo(event.offsetX, event.offsetY);
+    }
+}
+
+function endDraw(event) {
+    pencilMode = "";
+}
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", moveMouse);
+canvas.addEventListener("mouseup", endDraw);
